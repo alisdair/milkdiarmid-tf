405fde6d078e933699e2c40de379f72f24dda481
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2017-12-14 11:57:21 -0500

    Add DNSimple DNS

--- main.tf
+++ main.tf
@@ -10,6 +10,14 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+variable "dnsimple_account" {
+  type = "string"
+}
+
+variable "dnsimple_token" {
+  type = "string"
+}
+
 variable "web_count" {
   default = 1
 }
@@ -36,4 +44,17 @@ output "public_ip" {
 
 output "public_dns" {
   value = ["${aws_instance.web.*.public_ip}"]
+}
+
+provider "dnsimple" {
+  account = "${var.dnsimple_account}"
+  token = "${var.dnsimple_token}"
+}
+
+resource "dnsimple_record" "milkdiarmid" {
+  domain = "milkdiarmid.com"
+  type = "A"
+  name = "www"
+  ttl = 60
+  value = "${element(aws_instance.web.*.public_ip, count.index)}"
 }

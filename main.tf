ae2dc7d84efd5c47cd3241262b9886c74631001c
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2017-12-14 11:54:03 -0500

    Scaling and outputs

--- main.tf
+++ main.tf
@@ -10,13 +10,30 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+variable "web_count" {
+  default = 1
+}
+
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "web" {
+  count         = "${var.web_count}"
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+
+  tags {
+    "Name" = "web ${count.index+1}/${var.web_count}"
+  }
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_ip}"]
 }

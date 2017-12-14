a31a1827560ee9cc4a275031567373b5fcbb4783
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2017-12-14 11:40:01 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+variable "aws_access_key" {
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}

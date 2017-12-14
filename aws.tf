775988536e3574518b8264a8e66dcbd15fc551b2
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2017-12-14 13:05:27 -0500

    Split the config

---
+++ aws.tf
@@ -0,0 +1,35 @@
+variable "aws_access_key" {
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  count         = "${var.web_count}"
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+
+  tags {
+    "Name" = "web ${count.index+1}/${var.web_count}"
+  }
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
